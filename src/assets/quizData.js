d741278b05c082c48754ddceb21df98e447f763d
Chill Void <135580367+ChillVoid@users.noreply.github.com>
2026-01-06 15:32:37 +0800

    Update the usernames
    added for our trails

--- src/assets/quizData.js
+++ src/assets/quizData.js
@@ -1,4 +1,5 @@
 export const USERS = [
   { username: 'admin', password: 'admin12345', role: 'admin' },
   { username: '202311221', password: '123456789', role: 'student' },
+  { username: '202311211', password: '12345', role: 'student' }
 ];
